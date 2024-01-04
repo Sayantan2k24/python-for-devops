76c24661a672e8c343dd5525138bbe6bf3a40da6
Sayantan Samanta <90416470+Sayantan2k24@users.noreply.github.com>
2024-01-04 16:18:20 +0530

    Create env_vars.py

---
+++ Day-05/env_vars.py
@@ -0,0 +1,15 @@
+# export password="sayantan"
+# export api_token = "ahfgayr65@nfkaf0348ajds"
+# Till this on linux terminal or gitbash (if on windows) 
+
+
+# below is the code or programme
+import os
+
+print(os.getenv("password"))
+print(os.getenv("api_token"))
+
+
+# To execute in terminal
+# python env_vars.py
+
