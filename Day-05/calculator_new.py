6bf4f3b321714bb07dcb83073b20f0567f43c3ed
Sayantan Samanta <90416470+Sayantan2k24@users.noreply.github.com>
2024-01-04 16:29:12 +0530

    Update calculator_new.py

--- Day-05/calculator_new.py
+++ Day-05/calculator_new.py
@@ -16,12 +16,28 @@ def div(num1, num2):
     div = num1/num2
     return div
 
-num1 = float(sys.argv[1]) #sys.argv by default read the cmd line arg as string #so using typecasting into int 
+
+# num1 = int(sys.argv[1]) #sys.argv by default read the cmd line arg as string #so using typecasting into int 
+# operation = sys.argv[2]
+# num2 = int(sys.argv[3]) # problem will be if the user gives decimal values as the cmd line args; program will break
+
+num1 = float(sys.argv[1]) #sys.argv by default read the cmd line arg as string #so using typecasting into float 
 operation = sys.argv[2] 
-num2 = float(sys.argv[3]) # problem will be if the user gives decimal values as the cmd line args; program will break
+num2 = float(sys.argv[3]) # if the user gives decimal values as the cmd line args; program will work fine
 
 if operation == "add":
     output = add(num1, num2)
     print(output)
 
+
+# Execute the program -->
+
+# give integer values
+# python calculator_new.py 2 add 3
+# Output: 5
+
+
+# give decimal values
+# python calculator_new.py 2.6 add 3.5
+# Output: 6.1
 
